efe31dcf3f4cc9280b68c7e8c4ae33c5bed13315
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2023-07-19 12:19:34 +0100

    Replace classnames with smaller clsx library

--- src/Components/Notification/NotificationBar.tsx
+++ src/Components/Notification/NotificationBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classnames';
+import clsx from 'clsx';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import ErrorIcon from '@material-ui/icons/Error';
 import InfoIcon from '@material-ui/icons/Info';
@@ -59,11 +59,11 @@ function MySnackbarContent(props: MySnackbarContentProps) {
   return (
     open ?
       <SnackbarContent
-        className={classNames(classes[variant], className)}
+        className={clsx(classes[variant], className)}
         aria-describedby="client-snackbar"
         message={
           <span id="client-snackbar" className={classes.message}>
-            <Icon className={classNames(classes.icon, classes.iconVariant)} />
+            <Icon className={clsx(classes.icon, classes.iconVariant)} />
             {message}
           </span>
         }
